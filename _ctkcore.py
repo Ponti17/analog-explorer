b29c4127d41717513ee2efb0b4823408239c0b7c
andreasp <andreas@pedersenmail.com>
2024-04-12 08:56:32 +0200

    create gui file

--- _ctkcore.py
+++ _ctkcore.py
@@ -1,8 +1,12 @@
 import customtkinter as ctk
 
-class ctk_setup:
+class ctk_core:
     def init_ctk(self):
         ctk.set_appearance_mode("dark")
         self.root = ctk.CTk()
         self.root.geometry("1600x800")
         self.root.title("analog-py-designer")
+        
+    def ctk_run(self):
+        self.load_model()
+        self.root.mainloop()
