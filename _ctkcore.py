fdbaa33861e684b5b2156902b54fbfa161dadf99
andreasp <andreas@pedersenmail.com>
2024-04-16 20:53:30 +0200

    fix dual windows / exit errors

--- _ctkcore.py
+++ _ctkcore.py
@@ -10,7 +10,7 @@ class ctk_core:
         
     def init_ctk(self):
         ctk.set_appearance_mode("dark")
-        self.root = ctk.CTkToplevel()
+        self.root = ctk.CTk()
         self.root.geometry("1600x800")
         self.root.title("analog-py-designer")
         
@@ -20,4 +20,3 @@ class ctk_core:
 
     def quit(self):
         self.root.quit()
-        self.root.destroy()
