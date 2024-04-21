064b943414204eb557ce58b15350795cb09c2c0b
andreasp <andreas@pedersenmail.com>
2024-04-21 16:52:22 +0200

    disable input fitting
    find a better way to extract length and vds from titles. The order
    is not the same for all models.

--- _handledata.py
+++ _handledata.py
@@ -49,8 +49,8 @@ class dataHandler:
             self.len_vals.sort()
         
     def fit_vds_len(self, vds, length):
-        vds = self.vds_vals[min(range(len(self.vds_vals)), key = lambda i: abs(self.vds_vals[i]-vds))]
-        length = self.len_vals[min(range(len(self.len_vals)), key = lambda i: abs(self.len_vals[i]-length))]
+        # vds = self.vds_vals[min(range(len(self.vds_vals)), key = lambda i: abs(self.vds_vals[i]-vds))]
+        # length = self.len_vals[min(range(len(self.len_vals)), key = lambda i: abs(self.len_vals[i]-length))]
         return "{:.2e}".format(vds), "{:.2e}".format(length)
             
     def get_gmro(self, vds, length):
